8de56a0d55885b4e85597eb517fe7d1fce5aa4d0
joanne <jkim13@ufl.edu>
2024-11-03 22:30:55 -0500

    added progress line

--- src/main.cc
+++ src/main.cc
@@ -7,6 +7,7 @@ int main() {
     std::cout << "P3\n" << width << " " << height << "\n255\n";
 
     for (int j = 0; j < height; ++j) {
+        std::clog << "\rScanlines remaining: " << height - j << " " << std::flush;
         for (int i = 0; i < width; ++i) {
             double r = double(i) / (width - 1);
             double g = double(j) / (height - 1);
@@ -20,5 +21,7 @@ int main() {
         }
     }
 
+    std::clog  << "\rDone.                 \n";
+    
     return 0;
 }
