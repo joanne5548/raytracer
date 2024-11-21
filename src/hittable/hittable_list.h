f5e7923646a1f247bf7cfdc820e6b7d5abd5a24d
joanne <jkim13@ufl.edu>
2024-11-21 12:36:31 -0500

    modify hittable_list hit function

--- src/hittable/hittable_list.h
+++ src/hittable/hittable_list.h
@@ -4,10 +4,10 @@
 #include <vector>
 #include "hittable.h"
 
-class hittable_list {
-    std::vector<hittable> list;
-
+class hittable_list : public hittable {
     public:
+        std::vector<hittable> list;
+
         hittable_list() {}
 
         // list shouldn't be const since it can change
@@ -25,18 +25,28 @@ class hittable_list {
             list.push_back(object);
         }
 
-        // I guess there isn't really a good way to determine return type...
-        // I will follow the convention so far and return a bool
-        // TODO: change the return type
-        std::vector<hit_record> hit(const ray& r, double t_min, double t_max) {
-            std::vector<hit_record> hit_list;
+        // You actually just need one hitpoint!!! cause we only care about the frontmost intersection
+        bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override {
+            bool object_was_hit = false;
+            double closest_hit_t = t_max;
+
             for (int i = 0; i < list.size(); ++i) {
-                hit_record rec;
-                list.at(i).hit(r, t_min, t_max, rec);
-                hit_list.push_back(rec);
+                hit_record current_rec;
+
+                // If you use t to determine the distance, it's much faster than
+                // comparing the length of the vectors
+                if (list.at(i).hit(r, t_min, closest_hit_t, current_rec)) {
+                    // only store the closest hit record from the camera center
+                    if (current_rec.t < closest_hit_t) {
+                        rec = current_rec;
+                        closest_hit_t = current_rec.t;
+                    }
+
+                    object_was_hit = true;
+                }
             }
 
-            return hit_list;
+            return object_was_hit;
         }
 
 };
